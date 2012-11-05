2db8af5fbe179d75b09e81e00c69f36f1130eab9
Anas Nashif <anas.nashif@intel.com>
2012-11-04 18:09:59 -0800

    stdio.in fix

--- gnulib/lib/stdio.in.h
+++ gnulib/lib/stdio.in.h
@@ -178,9 +178,10 @@ _GL_WARN_ON_USE (fflush, "fflush is not always POSIX compliant - "
 /* It is very rare that the developer ever has full control of stdin,
    so any use of gets warrants an unconditional warning.  Assume it is
    always declared, since it is required by C89.  */
+#if HAVE_RAW_DECL_GETS
 #undef gets
 _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
-
+#endif
 #if @GNULIB_FOPEN@
 # if @REPLACE_FOPEN@
 #  if !(defined __cplusplus && defined GNULIB_NAMESPACE)
